4ad0d2837a09fde92ad965d3ecc3fe7c958d4343
Елена <simonaitene@yandex.ru>
2023-05-15 21:29:22 +0300

    Сделано ДЗ

---
+++ src/main/java/pro/sky/course2StreamAPIandoptional/DepartmentController.java
@@ -0,0 +1,36 @@
+package pro.sky.course2StreamAPIandoptional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+    private final DepartmentService service;
+    public DepartmentController(DepartmentService service){
+        this.service = service;
+    }
+    @GetMapping("/max-salary")
+    public Employee max(@RequestParam int departmentId){
+        return service.max(departmentId);
+    }
+    @GetMapping("/min-salary")
+    public Employee min(@RequestParam int departmentId){
+        return service.min(departmentId);
+    }
+    @GetMapping(value = "/all", params = " departmentId ")
+    public List<Employee> allByDept(@RequestParam int departmentId){
+        return service.allByDept(departmentId);
+    }
+    @GetMapping("/all")
+    public Map<Integer, List<Employee>> all(){
+        return service.all();
+    }
+
+}
---
+++ src/main/java/pro/sky/course2StreamAPIandoptional/EmployeeNotFoundException.java
@@ -0,0 +1,4 @@
+package pro.sky.course2StreamAPIandoptional;
+
+public class EmployeeNotFoundException extends RuntimeException{
+}
